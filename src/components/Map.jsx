90cde4b9a101fad49c2e8ab6f20348cd86c6a3c3
Phillip Cabrera <pcabreram1234@gmail.com>
2022-11-01 22:20:22 -0400

    Leaflet added to the React App

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import L from "leaflet";
 import "../../node_modules/leaflet/dist/leaflet.css";
 import "../styles/Map.css";
 
-export default function LeafletMap() {
-  useEffect(() => {
-    map();
-    console.log(mapRef);
-  }, []);
+const GOOGLE_URL_MAP = import.meta.env.VITE_GOOGLE_URL_MAP;
+const INIT_LAT = import.meta.env.VITE_DEFAULT_INIT_LAT;
+const INIT_LONG = import.meta.env.VITE_DEFAULT_INIT_LONG;
 
-  const map = () => {
-    const mapInstance = L.map("map_container").setView([51.505, -0.09], 13);
+export default function LeafletMap() {
+  const [map, setMap] = useState(null);
+
+  const initMap = () => {
+    let container = L.DomUtil.get("map_container");
+
+    if (container != null) {
+      container._leaflet_id = null;
+    }
+
+    let map = L.map(container).setView([INIT_LAT, INIT_LONG], 13);
+
+    L.tileLayer(GOOGLE_URL_MAP, {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="https://www.google.com/intl/en-GB_ALL/permissions/geoguidelines/">Google Maps</a>',
+    }).addTo(map);
+
+    map.locate({ setView: true, maxZoom: 16 });
+    // to use after the map is created, you can export the map object or use a ref
+
+    setMap(map);
+    return map;
   };
 
-  return <div id="map_container"></div>;
+  useEffect(() => {
+    initMap();
+  }, []);
+
+  return (
+    <div
+      id="map_container"
+      style={{ height: "100vh", touchAction: "manipulation" }}
+    ></div>
+  );
 }
